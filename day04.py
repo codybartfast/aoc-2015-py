c047fff8cc959c563b8d2a928999a256e914302f
codybartfast <john@codybartfast.com>
2026-01-27 20:03:19 +0000

    day4 part1

---
+++ day04.py
@@ -0,0 +1,41 @@
+import hashlib
+
+
+def parse(input):
+    return input
+
+
+def md5(key, number):
+    text = key + str(number)
+    return hashlib.md5(text.encode("ascii")).hexdigest()
+
+
+def search(key):
+    n = 0
+    while True:
+        hash = md5(key, n)
+        if hash[0:5] == "00000":
+            return n
+        n += 1
+
+
+def part1(key):
+    return search(key)
+
+
+def part2(data, ans1=None):
+    return "ans2"
+
+
+def jingle(filename=None, filepath=None, input=None):
+    import sack
+
+    input = input if input else sack.read_input(filename, filepath)
+    sack.present(input, parse, part1, part2)
+
+
+if __name__ == "__main__":
+    import sys
+
+    filename = sys.argv[1] if len(sys.argv) > 1 else None
+    jingle(filename=filename)
