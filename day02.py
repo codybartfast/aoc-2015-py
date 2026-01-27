0f09fd3dfe29ecb21b10c5574c4e00e237ac1ac6
codybartfast <john@codybartfast.com>
2026-01-27 15:38:32 +0000

    day2 part1

--- day02.py
+++ day02.py
@@ -1,10 +1,15 @@
 def parse(input):
-    return input
+    return [tuple(map(int, line.split("x"))) for line in input.splitlines()]
+
+
+def wrapping(dimensions):
+    d1, d2, d3 = sorted(dimensions)
+    s1, s2, s3 = d1 * d2, d2 * d3, d1 * d3
+    return s1 + 2 * (s1 + s2 + s3)
 
 
 def part1(data):
-    print(f"{data}\n")
-    return "ans1"
+    return sum(map(wrapping, data))
 
 
 def part2(data, ans1):
