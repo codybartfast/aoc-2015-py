32469fe50781d617a304a03f4d281a70e45e01dd
codybartfast <john@codybartfast.com>
2026-01-31 20:31:51 +0000

    day7 part2 - redone with objects

--- day07.py
+++ day07.py
@@ -1,6 +1,28 @@
 MAX_INT = 1 << 16
 
 
+class Gate:
+    def __init__(self, board, op, args):
+        self.board = board
+        self.op = op
+        self.args = args
+        self._value = None
+
+    def lookup(self, operand):
+        if operand.isdigit():
+            return int(operand)
+        return self.board[operand].value()
+
+    def calc(self):
+        vals = tuple(map(self.lookup, self.args))
+        return self.op(*vals)
+
+    def value(self):
+        if not self._value:
+            self._value = self.calc()
+        return self._value
+
+
 def parse(input):
     return [
         [expr.split(), wire]
@@ -8,68 +30,45 @@ def parse(input):
     ]
 
 
-def memo(cicuit):
-    values = {}
+ops = {
+    "ID": lambda a: a,
+    "NOT": lambda a: (~a) % MAX_INT,
+    "AND": lambda a, b: (a & b) % MAX_INT,
+    "OR": lambda a, b: (a | b) % MAX_INT,
+    "LSHIFT": lambda a, b: (a << b) % MAX_INT,
+    "RSHIFT": lambda a, b: (a >> b) % MAX_INT,
+}
 
-    def get(id):
-        if id not in values:
-            values[id] = cicuit[id]()
-        return values[id]
 
-    return get
-
-
-def wire_circuit(wire_info, b_val=-1):
-    circuit = {}
-    memo_get = memo(circuit)
-
-    def val_getter(txt):
-        if txt.isdigit():
-            val = int(txt)
-            return lambda val=val: val
-        else:
-            return lambda id=txt: memo_get(id)
+def wire_board(wire_info, b_val=-1):
+    board = {}
 
     for [parts, wire] in wire_info:
         if wire == "b" and b_val >= 0:
-            circuit["b"] = lambda: b_val
+            board["b"] = Gate(board, ops["ID"], (str(b_val),))
             continue
 
         if len(parts) == 1:
             txt = parts[0]
-            circuit[wire] = val_getter(txt)
+            board[wire] = Gate(board, ops["ID"], (txt,))
         elif len(parts) == 2:
             a = parts[1]
-            circuit[wire] = lambda a=a: (~circuit[a]()) % MAX_INT
+            board[wire] = Gate(board, ops["NOT"], (a,))
         else:
             [a, op, b] = parts
-            get_a, get_b = val_getter(a), val_getter(b)
-            match parts[1]:
-                case "AND":
-                    circuit[wire] = (
-                        lambda get_a=get_a, get_b=get_b: (get_a() & get_b()) % MAX_INT
-                    )
-                case "OR":
-                    circuit[wire] = (
-                        lambda get_a=get_a, get_b=get_b: (get_a() | get_b()) % MAX_INT
-                    )
-                case "LSHIFT":
-                    circuit[wire] = (
-                        lambda get_a=get_a, get_b=get_b: (get_a() << get_b()) % MAX_INT
-                    )
-                case "RSHIFT":
-                    circuit[wire] = lambda get_a=get_a, get_b=get_b: get_a() >> get_b()
-    return circuit
+            board[wire] = Gate(board, ops[op], (a, b))
+
+    return board
 
 
 def part1(wire_info):
-    circuit = wire_circuit(wire_info)
-    return circuit["a"]()
+    board = wire_board(wire_info)
+    return board["a"].value()
 
 
 def part2(wire_info, ans1=None):
-    circuit = wire_circuit(wire_info, ans1)
-    return circuit["a"]()
+    board = wire_board(wire_info, ans1)
+    return board["a"].value()
 
 
 def jingle(filename=None, filepath=None, input=None):
