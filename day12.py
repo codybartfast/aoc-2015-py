d26c332b773308c63f2d1b1e2e9a01f93c5273b8
codybartfast <john@codybartfast.com>
2026-02-03 17:10:53 +0000

    day12 part2

--- day12.py
+++ day12.py
@@ -1,6 +1,7 @@
 import json
 import re
 
+
 def parse(text):
     return text
 
@@ -9,8 +10,26 @@ def part1(data):
     return sum(int(m) for m in re.findall(r"-?\d+", data))
 
 
+def sum_nums(accts):
+    match accts:
+        case int():
+            return accts
+        case dict():
+            if "red" not in accts.values():
+                return sum(sum_nums(item) for item in accts.values())
+            else:
+                return 0
+        case list():
+            return sum(sum_nums(item) for item in accts)
+        case str():
+            return 0
+        case _:
+            raise RuntimeError(type(accts))
+
+
 def part2(data, ans1=None):
-    return "ans2"
+    return sum_nums(json.loads(data))
+
 
 def jingle(filename=None, filepath=None, text=None):
     import sack
