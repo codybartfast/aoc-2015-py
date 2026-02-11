677b518387e46ebafc7b9a782a0df2b2c13ace50
codybartfast <john@codybartfast.com>
2026-02-11 07:48:22 +0000

    day19 part2

--- day19.py
+++ day19.py
@@ -2,28 +2,39 @@ import re
 
 
 def parse(text):
+    elem_rx = re.compile(r"[A-Z][a-z]|[A-Z]")
     repl_txt, molecule = text.split("\n\n")
     replacements = {}
     for line in repl_txt.splitlines():
         parts = line.split()
-        replacements.setdefault(parts[0], []).append(parts[2])
-    return molecule, replacements
+        replacements.setdefault(parts[0], []).append(elem_rx.findall(parts[2]))
+    return elem_rx.findall(molecule), replacements
 
 
-def replace(molecule, replacements):
-    for match in re.finditer(r"[A-Z][a-z]|[A-Z]", molecule):
-        prefix = molecule[: match.start()]
-        suffix = molecule[match.end() :]
-        for repl in replacements.get(match.group(), []):
-            yield prefix + repl + suffix
+def replace_all(molecule, replacements):
+    for idx in range(len(molecule)):
+        prefix = molecule[:idx]
+        suffix = molecule[idx + 1 :]
+        for repl in replacements.get(molecule[idx], []):
+            yield "".join(prefix + repl + suffix)
 
 
 def part1(data):
-    return len(set(replace(*data)))
+    return len(set(replace_all(*data)))
 
 
 def part2(data, ans1=None):
-    return "ans2"
+    med, _ = data
+    remaining = len(med)
+    steps = 0
+
+    n_ar = med.count("Ar")
+    remaining -= 3 * n_ar
+    steps += n_ar
+
+    remaining -= 2 * med.count("Y")
+
+    return steps + remaining - 1
 
 
 def jingle(filename=None, filepath=None, text=None):
